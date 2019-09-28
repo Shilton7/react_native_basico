27e91a33265b16ea4719876077806dbd6132b3cf
Shilton <shilton.sistemas@gmail.com>
2019-09-27 23:27:41 -0300

    Aplicando Style

--- basico/src/pages/main.js
+++ basico/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Linking} from 'react-native';
 import api from '../services/api';
 
 export default class Main extends Component {
@@ -16,11 +16,11 @@ export default class Main extends Component {
   }
 
   renderItem = ({item}) => (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>{item.description}</Text>
-      <TouchableOpacity onPress={() => {}}>
-        <Text>Acessar</Text>
+    <View style={styles.containerProdutos}>
+      <Text style={styles.titulo_item}>{item.title}</Text>
+      <Text style={styles.desc_item}>{item.description}</Text>
+      <TouchableOpacity style={styles.btn_lista} onPress={() => Linking.openURL(item.url)}>
+        <Text style={styles.txtbtn}>Acessar</Text>
       </TouchableOpacity>
     </View>
   )
@@ -35,8 +35,9 @@ export default class Main extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <FlatList
+          contentContainerStyle={styles.listagem}
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
@@ -45,3 +46,48 @@ export default class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //ocupar tela toda
+    backgroundColor: "#fafafa"
+  },
+  listagem: {
+    padding: 20
+  },
+  containerProdutos: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+  titulo_item: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333"
+  },
+  desc_item: {
+    fontSize: 14,
+    color: "#999",
+    marginTop: 5,
+    lineHeight: 24
+  },
+  btn_lista: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "#7159c1",
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+
+  },
+  txtbtn: {
+    fontSize: 16,
+    color: "#7159c1",
+    fontWeight: "bold"
+  }
+});
